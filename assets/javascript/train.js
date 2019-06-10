692ed98c5591749509e958bab00140e51362ab0a
Cecilia Leon <leoncecilia17@gmail.com>
2019-06-09 23:13:42 -0500

    linking css, bootstrap, jquery to html

---
+++ assets/javascript/train.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCuTKPv6T9VLzI8lnnrrPLtMfRBW0Nuxz8",
+    authDomain: "trainschedule-14a1c.firebaseapp.com",
+    databaseURL: "https://trainschedule-14a1c.firebaseio.com",
+    projectId: "trainschedule-14a1c",
+    storageBucket: "trainschedule-14a1c.appspot.com",
+    messagingSenderId: "549143950962",
+    appId: "1:549143950962:web:8373f95d3024b192"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database(); 
+
+}); 
